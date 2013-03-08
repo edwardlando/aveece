fbd2188787c0df0040448d2cb265159d2c276a4a
edwardlando <edwardlando8@gmail.com>
2013-03-08 02:22:37 -0500

    added masonry jquery and infinite scroll (from masonry as well); for now this breaks the javascript and has to be fixed

--- app/assets/javascripts/aveece.js
+++ app/assets/javascripts/aveece.js
@@ -1,18 +1,7 @@
 
 $(document).ready(function() {
 
-    /*
-    // Masonry
-	var $container = $('#container');
-
-	$container.imagesLoaded(function(){
-	  $container.masonry({
-	    itemSelector : '.box'
-	  });
-	});
-    */
-
-    // Stops Devise from signing out user when Ajax POST request is made
+	// Stops Devise from signing out user when Ajax POST request is made
 	$.ajaxSetup({
 	    beforeSend: function(xhr) {
 	        xhr.setRequestHeader('X-CSRF-Token',
@@ -23,6 +12,29 @@ $(document).ready(function() {
 	jQuery.ajaxSetup({ 
 		'beforeSend': function(xhr) {xhr.setRequestHeader("Accept", "text/javascript")} 
 	});
+
+    // Masonry for images
+	var $container = $('#container');
+
+	$container.imagesLoaded(function(){
+	  $container.masonry({
+	    itemSelector : '.box'
+	  });
+	});
+   
+
+    // Infinite scroll 
+    var $container = $('#container');
+	$container.infinitescroll({
+	    // infinite scroll options...
+	  },
+	  // trigger Masonry as a callback
+	  function( newElements ) {
+	    var $newElems = $( newElements ); // need to get the new elements with a GET request
+	    $container.masonry( 'appended', $newElems );
+	  }
+	);
+
 
 	// Modals
 	var overlayed = true;
